a055ee676415e5e34bb3dddecb2da538dc0b09d4
Adrian Cornely <acornely@ben.edu>
2022-01-26 11:00:20 -0600

    Gitignore and intro

---
+++ intro/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    //primitive types
+    char a = 'a';
+    //32 bits of precision 
+    int integer = 8;
+    //64 bits of precision
+    long l = 8;
+    byte b = -128;
+    //16 bits of precision
+    short s = (short) 2.5;
+    //32 bits of precision
+    float f = 2.5f;
+    //64 bits of precision
+    double d = 2.5;
+    boolean bool = true;
+
+    static String str = "fkjdsaf;kjsajfoirejfoeqj;ofj";
+
+    public static void main(String[] args) {
+        str = str + "a";
+        System.out.println(str);
+    }
+    //this is a comment
+}
