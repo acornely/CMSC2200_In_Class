2dc811689c858ee6ccd7a14c53eaa227e36ef08c
Adrian Cornely <acornely@ben.edu>
2022-02-02 14:54:19 -0600

    In class work

--- objects/Dog.java
+++ objects/Dog.java
@@ -1,11 +1,26 @@
 public class Dog {
     String name;
-    boolean hasTail;
+    static boolean hasTail = true;
+
+    //default constructor
+    public Dog() {
+        
+    }
+
+    //parameterized constructor 
+    public Dog(String name) {
+        //here name = "Spike" at line 17
+        this.name = name;
+    }
 
     public static void main(String[] args) {
-        Dog myDog = new Dog();
-        myDog.name = "Spike";
-        myDog.hasTail = true;
+        Dog myDog = new Dog("Spike");
+        Dog myDog2 = new Dog("Spot");
+        Dog myDog3 = new Dog("kdshkasjfdlhsafd");
         System.out.println(myDog.name);
+        System.out.println(myDog2.name);
+        System.out.println(myDog3.name);
+        System.out.println(Dog.hasTail);
+
     }
 }
