e8c42e83ac400603495881968be4daf472fbefe4
Adrian Cornely <acornely@ben.edu>
2022-03-09 14:54:02 -0600

    Loop example

---
+++ loops/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String[] args) {
+        for(int i = 10; i >= 1; i-=2) {
+            System.out.println(i);
+        }
+        int i = 10;
+        while(i >= 0) {
+            System.out.println(i);
+            i-=2;
+        }
+    }
+}
